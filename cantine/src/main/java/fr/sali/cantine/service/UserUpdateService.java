5b11d06cbb154ea1d5219edcc8bc8353ad0b18b0
Akirabane <joshuabenon988@yahoo.fr>
2022-11-09 11:35:19 +0100

    implemented UserUpdateService

---
+++ cantine/src/main/java/fr/sali/cantine/service/UserUpdateService.java
@@ -0,0 +1,22 @@
+package fr.sali.cantine.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("users")
+public class UserUpdateService {
+
+    @Autowired
+    private UserUpdateService userUpdateService;
+
+    @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    public void updateUser() {
+        userUpdateService.updateUser();
+    }
+
+}
