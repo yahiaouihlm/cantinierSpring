08f4ec008097e39684ec43d7a545fb11bf380a13
halim <yahiaouihlm@gmail.com>
2022-11-09 12:09:04 +0100

    ajouter la notation  repository

--- cantine/src/main/java/fr/sali/cantine/dao/IImageDao.java
+++ cantine/src/main/java/fr/sali/cantine/dao/IImageDao.java
@@ -3,5 +3,6 @@ package fr.sali.cantine.dao;
 import fr.sali.cantine.entity.ImageEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IImageDao extends JpaRepository<ImageEntity ,  Integer> {
+@Repository
+public interface IImageDao extends JpaRepository<ImageEntity, Integer> {
 }
